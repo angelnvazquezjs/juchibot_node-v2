2057f2f905b1084d416111a4c1b13631442db740
Angel Vazquez <angelnvazquez14@gmail.com>
2022-05-29 17:43:35 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,29 @@
+// Importamos la API de telegraf
+const { Telegraf } = require('telegraf');
+
+// Hacemos la conexion con la API
+const bot = new Telegraf('5140540597:AAF3UGfzsgxGh0ULM2ifqOSevgcRla1hy-w');
+
+// Comandos para JuchiBot
+bot.start( ctx => {
+    ctx.reply('Hola Juchiman, puedes proporcionarme tu matricula?');
+});
+
+bot.help( ctx => {
+    ctx.reply('En que te puedo ayudar?');
+});
+
+bot.command('reinscripcion', ctx => {
+    ctx.reply('Comando de Reinscripcion!!');
+});
+
+bot.hears('Hola', ctx => {
+    ctx.reply('Hey, soy JuchiBot, tu asistente Universitario. Para poder brindarte mi ayuda, necesito que escribas cualquiera de los comandos.');
+});
+
+bot.on('text', ctx => {
+    ctx.reply('Estas texteando');
+});
+
+// Inicia el Bot
+bot.launch();
